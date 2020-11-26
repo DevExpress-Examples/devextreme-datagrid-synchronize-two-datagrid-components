bba5dc100a8f28976602676d483591c0ec543dfe
Luis Capuyon <frostehhh967@gmail.com>
2020-11-26 18:39:09 +0800

    React - Implement dynamic synced opts

--- React/sample/src/App.js
+++ React/sample/src/App.js
@@ -1,6 +1,6 @@
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
-import React, { useCallback, useState, useReducer } from 'react';
+import React, { useCallback, useState, useReducer, useMemo, useEffect } from 'react';
 import './App.css';
 import Grid from "./components/Grid"
 import { customers } from "./data"
@@ -8,28 +8,29 @@ import reducer from './logic/reducer'
 import initState from './logic/initState'
 import gridEnum from './gridEnum'
 
+
 function App() {
-  const [{one, two}, dispatch] = useReducer(reducer, initState)
+  const [state, dispatch] = useReducer(reducer, initState)
+  const getSyncedGridOptions = useCallback((grid) => {
+    return {
+      selectedRowKeys: state[grid].selectedRowKeys,
+      pageSize: state[grid].pageSize,
+      pageIndex: state[grid].pageIndex,
+      handleSelectedRowKeysChange: (keys) => dispatch({type: "selectedRowKeys", value: keys, grid})
+    }
+  }, [state])
 
   return (
     <div className="App">
       <div className="tables">
         <div className="column">
           <Grid dataSource={customers} 
-            selectedRowKeys={one.selectedRowKeys}
-            pageSize={one.pageSize}
-            handleSelectedRowKeysChange={keys => dispatch({type: "selectedRowKeys", value: keys, grid: gridEnum.one})}
-            handlePageSizeChange={pageSize => dispatch({type: "pageSize", value: pageSize, grid: gridEnum.one})}
-            handlePageIndexChange={pageIndex => dispatch({type: "pageSize", value: pageIndex, grid: gridEnum.one})}
+            syncedOpts={getSyncedGridOptions(gridEnum.one)}
           />
         </div>
         <div className="column">
           <Grid dataSource={customers} 
-            selectedRowKeys={two.selectedRowKeys}
-            pageSize={two.pageSize}
-            handleSelectedRowKeysChange={(keys) => dispatch({type: "selectedRowKeys", value: keys, grid: gridEnum.two})}
-            handlePageSizeChange={pageSize => dispatch({type: "pageSize", value: pageSize, grid: gridEnum.two})}
-            handlePageIndexChange={pageIndex => dispatch({type: "pageSize", value: pageIndex, grid: gridEnum.two})}
+            syncedOpts={getSyncedGridOptions(gridEnum.two)}
           />
         </div>
       </div>
