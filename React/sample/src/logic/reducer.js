9de0c47e4f728ca1c3ef278c08eac640c1f9b8a8
Luis Capuyon <frostehhh967@gmail.com>
2020-12-10 15:29:29 +0800

    React - Simplified reducer

--- React/sample/src/logic/reducer.js
+++ React/sample/src/logic/reducer.js
@@ -1,33 +1,25 @@
 import gridEnum from '../data/gridEnum'
 
 function reducer(state, action){
-  const {grid, prop, value, column, getUpdatedOptions} = action
-  let gridsArr = [gridEnum.two],
+  const {grid, prop, value, column} = action,
+    gridsArr = [gridEnum.two],
     newState = {...state};
 
   switch(action.type){
-    case 'UPDATE_SINGLE_OPTION':         
+    case 'UPDATE_OPTION':
       if(grid === gridEnum.one) 
         gridsArr.push(gridEnum.one);
-      
+
       gridsArr.forEach(grid => {
-        newState[grid] = getUpdatedOptionState(state, prop, value, grid);
+        newState[grid] = column ? getUpdatedColumnOptionState(state, prop, column, value, grid) : getUpdatedOptionState(state, prop, value, grid);
       })
 
       return newState
-    case 'UPDATE_COLUMN_OPTION':
-      if(grid === gridEnum.one) 
-        gridsArr.push(gridEnum.one);
-
-      gridsArr.forEach(grid => {
-        newState[grid] = getUpdatedColumnOptionState(state, prop, column, value, grid)
-      })
-
-      return newState;
     default:
       break;
   }
 }
+
 
 function getUpdatedOptionState(state, prop, value, grid) {
   return {
@@ -47,13 +39,12 @@ function getUpdatedColumnOptionState(state, prop, column, value, grid) {
 }
 
 function updateOptions(prop, value, grid) {
-  return {type: 'UPDATE_SINGLE_OPTION', prop, value, grid}
+  return {type: 'UPDATE_OPTION', prop, value, grid}
 }
 
 function updateColumnOptions(prop, column, value, grid) {
-  return {type: 'UPDATE_COLUMN_OPTION', prop, column, value, grid}
+  return {type: 'UPDATE_OPTION', prop, column, value, grid}
 }
-
 
 export default reducer;
 export { updateOptions, updateColumnOptions }
