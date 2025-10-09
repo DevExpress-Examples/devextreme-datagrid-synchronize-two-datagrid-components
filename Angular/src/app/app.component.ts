6beb92b41e0df51ac173d5855fec9f002c32d999
flagmanAndrew <andrey.sergeyev@devexpress.com>
2025-10-09 19:20:25 +0400

    Angular updated

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,20 +1,63 @@
-import { Component } from '@angular/core';
-import { ClickEvent } from 'devextreme/ui/button';
+import { Component, ViewChild } from '@angular/core';
+import { DxDataGridComponent, DxDataGridTypes } from 'devextreme-angular/ui/data-grid';
+import { DxDataGridModule } from 'devextreme-angular';
+import { DxScrollViewTypes } from 'devextreme-angular/ui/scroll-view';
+import { Customer, Service } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [DxDataGridModule],
+  providers: [Service],
 })
 export class AppComponent {
-  title = 'Angular';
+  @ViewChild('dependentGrid') dependentGrid!: DxDataGridComponent;
 
-  counter = 0;
+  customers: Customer[];
 
-  buttonText = 'Click count: 0';
+  dependentScrollable: DxDataGridTypes.Scrollable | null = null;
 
-  onClick(e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  pageSize = 10;
+
+  allowedPageSizes: number[] = [5, 10, 20];
+
+  pageIndex = 0;
+
+  selectedRowKeys: number[] = [];
+
+  columnFilterValues: any = {};
+
+  columnSelectedFilterOps: any = {};
+
+  columnSortOpts: any = {};
+
+  constructor(service: Service) {
+    this.customers = service.getCustomers();
+  }
+
+  onOptionChanged(e: DxDataGridTypes.OptionChangedEvent): void {
+    if (e.fullName.includes('sortOrder')) {
+      const match = /\[(\d+)\]/.exec(e.fullName);
+      const colIdx = match ? parseInt(match[1], 10) : null;
+      if (colIdx) {
+        const dataField = e.component.columnOption(colIdx, 'dataField');
+        this.dependentGrid.instance.clearSorting();
+        this.columnSortOpts = {};
+        this.columnSortOpts[dataField] = e.value;
+      }
+    }
+  }
+
+  onMainGridReady(e: DxDataGridTypes.ContentReadyEvent): void {
+    const that = this;
+    e.component.getScrollable().on('scroll', (e: DxScrollViewTypes.ScrollEvent) => {
+      that.dependentScrollable?.scrollTo(e.scrollOffset);
+    });
+  }
+
+  onDependentGridReady(e: DxDataGridTypes.ContentReadyEvent): void {
+    this.dependentScrollable = e.component.getScrollable();
   }
 }
